8b2111ff84d1023ef77904d6b724f094e2c4546c
misohan <57062352+misohan@users.noreply.github.com>
2020-03-11 20:44:19 +0100

    Update Main for testing classes

--- Main.java
+++ Main.java
@@ -7,7 +7,8 @@ public class Main{
 	public static void main(String[] args) throws IOException {
 		CardParser parser = new CardParser();
 
-		parser.readFileTo2DList("Cards.csv").size();
+		System.out.println(parser.readFileTo2DList("Cards.csv"));
+		System.out.println(parser.readFileToList("Cards.csv"));
 		
 		
 	}
